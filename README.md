702a2f90b61d67bb3408d6ee6c0291647f0a319c
Mohit Khare <mohit.khare@go-jek.com>
2020-02-28 15:25:57 +0530

    Add I am rich app

---
+++ README.md
@@ -0,0 +1,6 @@
+# Play with Flutter
+
+- Cool and simple apps, Build while learning flutter.
+
+Author - [Mohit Khare](https://github.com/mkfeuhrer)
+
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
